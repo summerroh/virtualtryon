2ea78cdfe9f3e05e37600f057cc07aea0c581160
Summer Roh <summerrohh@gmail.com>
2024-05-17 01:50:48 +0900

    chore: Refactor deleteUser function to handle session storage in browser only

--- components/functions/deleteUser.jsx
+++ components/functions/deleteUser.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useEffect } from "react";
 import { useDeleteUser, useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
@@ -8,36 +6,30 @@ import { useRouter } from "next/navigation";
 export default function DeleteUserFunction() {
   const [user] = useAuthState(auth);
   const router = useRouter();
-  const userSession = sessionStorage.getItem("user");
 
   const [deleteUser, loading, error] = useDeleteUser(auth);
-
-  //   if (error) {
-  //     return (
-  //       <div>
-  //         <p>Error: {error.message}</p>
-  //       </div>
-  //     );
-  //   }
-  //   if (loading) {
-  //     return <p>Loading...</p>;
-  //   }
 
   const handleDeleteUser = async () => {
     const success = await deleteUser();
     if (success) {
-      sessionStorage.removeItem("user");
-      alert("User have been deleted");
+      if (typeof window !== "undefined") {
+        sessionStorage.removeItem("user");
+      }
+      alert("User has been deleted");
       router.push("/");
     }
   };
 
   useEffect(() => {
-    // 로그인 안되어 있으면 로그인 페이지로 이동
-    if (!user && !userSession) {
-      router.push("/login");
+    // Only access sessionStorage if running in the browser
+    if (typeof window !== "undefined") {
+      const userSession = sessionStorage.getItem("user");
+      // 로그인 안되어 있으면 로그인 페이지로 이동
+      if (!user && !userSession) {
+        router.push("/login");
+      }
     }
-  }, [user, userSession, router]);
+  }, [user, router]);
 
   return (
     <>
