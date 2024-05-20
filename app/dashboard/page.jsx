882a0fa85f9d1336167f40393d32f313e062e3fc
Summer Roh <summerrohh@gmail.com>
2024-05-21 01:31:15 +0900

    drag and drop

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -31,9 +31,9 @@ import { Menu } from "@/components/menu";
 import { PodcastEmptyPlaceholder } from "@/components/podcast-empty-placeholder";
 import { Sidebar } from "@/components/sidebar";
 import { listenNowAlbums, madeForYouAlbums } from "@/data/albums";
-import { playlists } from "@/data/playlists";
 
 import musicLight from "@/public/examples/music-light.png";
+import DragNDrop from "@/components/DragNDrop";
 
 export const metadata = {
   title: "Virtual Fitting Room",
@@ -44,15 +44,14 @@ export default function Dashboard() {
   return (
     <>
       <div className="block">
-        <Menu />
         <div className="border-t">
           <div className="bg-background">
-            <div className="grid lg:grid-cols-5">
-              <Sidebar playlists={playlists} className="block" />
-              <div className="col-span-3 lg:col-span-4 lg:border-l">
+            <div className="grid lg:grid-cols-6">
+              <Sidebar className="block" />
+              <div className="col-span-3 lg:col-span-5 lg:border-l">
                 <div className="h-full px-4 py-6 lg:px-8">
                   <Tabs defaultValue="music" className="h-full space-y-6">
-                    <div className="space-between flex items-center">
+                    {/* <div className="space-between flex items-center">
                       <TabsList>
                         <TabsTrigger value="music" className="relative">
                           Music
@@ -68,11 +67,26 @@ export default function Dashboard() {
                           Add music
                         </Button>
                       </div>
-                    </div>
+                    </div> */}
+
                     <TabsContent
                       value="music"
                       className="border-none p-0 outline-none"
                     >
+                      <div className="flex items-center justify-between">
+                        <div className="space-y-1">
+                          <p className="text-sm text-muted-foreground">
+                            Step 1/5
+                          </p>
+                          <h2 className="text-2xl font-semibold tracking-tight">
+                            Start with a photo of your cloth
+                          </h2>
+                        </div>
+                      </div>
+                      <div className="relative">
+                        <DragNDrop />
+                      </div>
+
                       <div className="flex items-center justify-between">
                         <div className="space-y-1">
                           <h2 className="text-2xl font-semibold tracking-tight">
@@ -83,7 +97,6 @@ export default function Dashboard() {
                           </p>
                         </div>
                       </div>
-                      <Separator className="my-4" />
                       <div className="relative">
                         <ScrollArea>
                           <div className="flex space-x-4 pb-4">
@@ -101,6 +114,7 @@ export default function Dashboard() {
                           <ScrollBar orientation="horizontal" />
                         </ScrollArea>
                       </div>
+
                       <div className="mt-6 space-y-1">
                         <h2 className="text-2xl font-semibold tracking-tight">
                           Made for You
@@ -109,7 +123,6 @@ export default function Dashboard() {
                           Your personal playlists. Updated daily.
                         </p>
                       </div>
-                      <Separator className="my-4" />
                       <div className="relative">
                         <ScrollArea>
                           <div className="flex space-x-4 pb-4">
