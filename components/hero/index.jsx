3d39fc81a3af25d1d956806bf0986977562bd0d5
Summer Roh <summerrohh@gmail.com>
2024-06-09 20:24:57 +0900

    hero image replaced

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -4,7 +4,7 @@ import Partners from "./Partners";
 
 const index = () => {
   return (
-    <div className="position-relative zn2 pt-1 bg-background-hero">
+    <div className="position-relative bg-background-hero pt-1">
       <div className="flex justify-center px-8 md:px-24 lg:px-24 xl:px-28 2xl:px-52 bg-background-hero mt-12">
         <div className="w-full space-y-12 md:space-y-24 flex flex-col md:flex-row justify-between items-center sm:gap-10 lg:gap-24">
           <div className="" data-aos="fade-right">
