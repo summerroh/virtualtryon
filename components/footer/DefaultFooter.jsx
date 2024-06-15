10fedc6a7b7d16d3fee9b698d290ff5cd76589f6
Summer Roh <summerrohh@gmail.com>
2024-06-15 16:21:16 +0900

    removed unused images

--- components/footer/DefaultFooter.jsx
+++ components/footer/DefaultFooter.jsx
@@ -5,43 +5,32 @@ import CopyrightFooter2 from "./CopyrightFooter2";
 
 const DefaultFooter = () => {
   return (
-    <div className="footer-style-eleven theme-basic-footer position-relative">
-      <div className="bg-wrapper position-relative">
-        <div className="container">
-          <div className="row justify-content-between">
-            <div className="col-xl-2 footer-intro mb-40">
+    <div className="relative footer-style-eleven theme-basic-footer">
+      <div className="relative bg-wrapper">
+        <div className="container mx-auto">
+          <div className="flex flex-wrap justify-between">
+            <div className="w-full xl:w-2/12 mb-10">
               <div className="logo">
                 <Link href="/">
                   <img src="/images/logo/logo_01.png" alt="brand" />
                 </Link>
               </div>
             </div>
-            {/* End .col */}
 
             <Footer2 />
 
-            <div className="col-xl-4 col-lg-5 mb-30 form-widget">
-              <h5 className="footer-title text-dark fw-normal">Newslettert</h5>
-              <h6 className="pt-15 pb-20 md-pt-10">Join our newsletter</h6>
+            <div className="w-full xl:w-4/12 lg:w-5/12 mb-10">
+              <h5 className="text-dark font-normal">Newsletter</h5>
+              <h6 className="pt-4 pb-5 md:pt-2">Join our newsletter</h6>
               <NewsLetter />
-              <div className="fs-14 mt-10">
+              <div className="text-sm mt-2">
                 We only send interesting and relevant emails.
               </div>
             </div>
-            {/* End .col-xl-4 */}
           </div>
-          {/* End .row */}
         </div>
-        {/* /.container */}
       </div>
-      {/* /.bg-wrapper */}
       <CopyrightFooter2 />
-      {/* /.bottom-footer */}
-      <img
-        src="/images/shape/shape_173.svg"
-        alt="shape"
-        className="lazy-img shapes shape-one"
-      />
     </div>
   );
 };
