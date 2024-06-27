0473356e4d6963024140d82771535e113690cdbd
Summer Roh <summerrohh@gmail.com>
2024-06-27 16:50:51 +0900

    privacy policy & terms of service pages added

---
+++ app/terms/page.jsx
@@ -0,0 +1,202 @@
+import CopyrightFooter from "@/components/landing/CopyrightFooter";
+import Header from "@/components/landing/Header";
+
+export const metadata = {
+  title: "Virtual Try On || Create professional fashion photoshoots",
+};
+export default function Terms() {
+  return (
+    <>
+      <Header />
+
+      {/* Contents */}
+      <div className="w-full pb-20 bg-background-hero flex justify-center items-center">
+        <div className="flex flex-col items-center gap-2 lg:gap-6 p-0 md:p-8">
+          <h1 className="mb-12 mt-32 md:mb-32 fw-normal text-white font-recoleta text-4xl lg:text-6xl max-w-[700px] text-center">
+            Terms of Service
+          </h1>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Introduction
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Welcome to Virtual Try On (“Company”, “we”, “our”, “us”). These
+              Terms of Service (“Terms”) govern your use of our SaaS platform
+              for virtual try-on (“Service”). By accessing or using our Service,
+              you agree to comply with and be bound by these Terms. If you do
+              not agree with these Terms, you should not use our Service.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Account Terms
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Account Creation: To use our Service, you must create an account
+              by providing accurate and complete information. You are
+              responsible for maintaining the confidentiality of your account
+              credentials and for all activities that occur under your account.
+              <br />
+              <br />
+              User Responsibilities: You agree to use the Service only for
+              lawful purposes and in accordance with these Terms. You are
+              responsible for ensuring that your use of the Service does not
+              violate any applicable laws or regulations.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Service Usage
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Service Description: Our Service provide AI-generated images. The
+              Service includes software, tools, and related support.
+              <br />
+              <br />
+              Prohibited Uses: You agree not to use the Service to engage in any
+              illegal activities, infringe on any third party’s rights, or
+              distribute malicious software. Any unauthorized use of the Service
+              may result in termination of your account.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Payment Terms
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Pricing and Billing: The pricing for our Service is set forth on
+              our website or in your order agreement. You agree to pay all fees
+              as specified. We reserve the right to change our pricing at any
+              time with notice to you.
+              <br />
+              <br />
+              Payment Terms: Payments are due according to the terms set forth
+              in your order agreement. Late payments may result in suspension or
+              termination of your access to the Service.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Intellectual Property
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Ownership: We retain all rights, title, and interest in and to the
+              Service, including all software, content, and related intellectual
+              property rights.
+              <br />
+              <br />
+              License: We grant you a limited, non-exclusive, non-transferable
+              license to use the Service solely for your internal business
+              purposes.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Confidentiality
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Confidential Information: You agree to maintain the
+              confidentiality of all confidential information disclosed to you
+              by us, and not to use or disclose such information except as
+              permitted under these Terms.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Data Protection
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Compliance: We comply with applicable data protection laws. By
+              using our Service, you consent to the collection, use, and sharing
+              of your data as described in our Privacy Policy.
+              <br />
+              <br />
+              User Consent: You represent that you have obtained all necessary
+              consents from your end-users for the collection, use, and sharing
+              of their data as required by applicable laws.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Termination
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Termination by You: You may terminate your account at any time by
+              contacting us.
+              <br />
+              <br />
+              Termination by Us: We may terminate your access to the Service if
+              you violate these Terms or if we cease offering the Service.
+              <br />
+              <br />
+              Effects of Termination: Upon termination, your right to use the
+              Service will immediately cease, and any data associated with your
+              account may be deleted.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Limitation of Liability
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              Disclaimer of Warranties: The Service is provided “as is” and “as
+              available” without warranties of any kind. We disclaim all
+              warranties, whether express or implied.
+              <br />
+              <br />
+              Limitation of Liability: To the maximum extent permitted by law,
+              we will not be liable for any indirect, incidental, special,
+              consequential, or punitive damages, or any loss of profits or
+              revenues.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Changes to Terms
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              We may modify these Terms at any time by posting the revised Terms
+              on our website. Your continued use of the Service after such
+              changes constitutes your acceptance of the new Terms.
+            </p>
+          </div>
+
+          <div className="space-y-2 md:space-y-2 flex flex-col gap-2 lg:gap-6 p-8">
+            <h1 className="fw-normal text-white font-recoleta text-xl lg:text-2xl text-left">
+              Contact Information
+            </h1>
+
+            <p className="text-md lg:text-lg text-secondary max-w-[800px]">
+              If you have any questions about these Terms, please contact us at:
+              virtualtryon.io@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative">
+        <CopyrightFooter />
+      </div>
+    </>
+  );
+}
